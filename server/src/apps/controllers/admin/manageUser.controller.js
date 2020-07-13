af7a8856488a472fa5996d78fd7abe0b7e9e21fc
Hieu Tran <hieucr98tb@gmail.com>
2020-07-13 17:32:54 +0700

    add userManagecontroller

--- server/src/apps/controllers/admin/manageUser.controller.js
+++ server/src/apps/controllers/admin/manageUser.controller.js
@@ -1,6 +1,83 @@
+const mongoose = require("mongoose");
+const joi = require("@hapi/joi");
+const User = mongoose.model("users");
+const pagination = require("./../../../lib/pagination");
+const { BadRequestException } = require("../../exceptions/index")
 
-
-
-module.exports.test = (req, res) => {
-    res.send('Ok')
+module.exports.user = async (req, res, next) => {
+  try {
+    const { limit, currentPage, skip } = pagination.index(req);
+    const totalDocument = await User.find().countDocuments();
+    const totalPages = Math.ceil(totalDocument / limit);
+    // console.log(totalPages);
+    const users = await User.find()
+      .sort("-_id")
+      .limit(limit)
+      .skip(skip)
+    return res.status(200).json({ status: "success", data: users, items: { totalDocument, limit }, pages: { currentPage} });
+  } catch (error) {
+    next(error)
+  }
+};
+module.exports.add_user = async (req, res) => {
+  const bodySchema = joi
+    .object({
+      email: joi.string().required(),
+      name: joi.string().required(),
+    })
+    .unknown();
+   const value = await bodySchema.validateAsync(req.body).catch((err) => err);
+   if ( value instanceof Error) {
+      return res.status(400).json({message: " Not add user !!!"})
+   }
+    const userNew = new User({
+      email: value.email,
+      name: value.name,
+      password: value.password,
+      authType: value.authType,
+      authGoogleID: value.authGoogleID,
+      authFacebookID: value.authFacebookID,
+      role: value.role,
+      phoneNumber: value.phoneNumber,
+    });
+   await userNew.save();
+  return res.status(201).json({ status: "success", user: userNew});
+};
+module.exports.get_user = async (req, res) => {
+    const { id } = req.params;  
+    const user = await User.findById(id);
+  return res.status(201).json({ status: "success", user });
 }
+module.exports.edit_user = async (req, res) => {
+    const { id } = req.params;
+     const bodySchema = joi
+    .object({
+      email: joi.string().required(),
+      name: joi.string().required(),
+    })
+    .unknown();
+   const value = await bodySchema.validateAsync(req.body).catch((err) => err);
+   if ( value instanceof Error) {
+      return res.status(400).json({message: " Not edit user !!!"})
+   }
+    const userUpdate = {
+      email: value.email,
+      name: value.name,
+      password: value.password,
+      authType: value.authType,
+      authGoogleID: value.authGoogleID,
+      authFacebookID: value.authFacebookID,
+      role: value.role,
+      phoneNumber: value.phoneNumber,
+    };   
+    await User.updateOne({ _id: id }, userUpdate);
+    return res.status(200).json({success: true})  
+};
+module.exports.delete_user = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: " Not delete user !!!" })
+  }
+  const user = await User.findByIdAndDelete(id);
+  return res.status(200).json({ success: true })
+};
--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -3,9 +3,16 @@ const AdminController = require("../apps/controllers/admin/manageUser.controller
 const checkLogin = require("../apps/middlewares/checkLogin")
 
 const apiRouter = Router()
-apiRouter.use('/admin', checkLogin.Login)
-apiRouter.route("/admin")
-    .get(AdminController.test)
-
-
-module.exports = apiRouter
+//apiRouter.use('/admin', checkLogin.Login)
+apiRouter.route("/admin/user")
+    .get(AdminController.user)
+apiRouter.route("/admin/user/add")
+    .post(AdminController.add_user)
+  //  .post(upload.single("prd_image"), AdminController.store);
+apiRouter.route("/admin/user/edit/:id")
+    .get(AdminController.get_user)
+    .put(AdminController.edit_user)
+ //   .post(upload.single("prd_image"), AdminController.update_user);
+apiRouter.route("/admin/user/delete/:id")
+    .delete(AdminController.delete_user);
+module.exports = apiRouter;
