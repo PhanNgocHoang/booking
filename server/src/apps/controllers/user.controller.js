1cbbc977ec7427919e02098e787a7e385f125a21
emlahieu98 <hieucr98tb@gmail.com>
2020-07-30 17:28:08 +0700

    fix error auth

--- server/src/apps/controllers/user.controller.js
+++ server/src/apps/controllers/user.controller.js
@@ -16,30 +16,31 @@ exports.authentication = catchAsync(async (req, res) => {
       exp: new Date().setDate(new Date().getDate() + 3)
     }, jwt_secret)
   }
-  module.exports.register = async function (req, res, next) {
-    const { name, password, email, role , phoneNumber } = req.body;
+module.exports.register = async function (req, res, next) {
+  try {
+    const { name, password, email, role, phoneNumber } = req.body;
     const bodySchema = joi.object({
-        name: joi.string()
-            .min(3)
-            .max(30)
-            .required(),
-        password: joi.string()
-            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-        email: joi.string()
-            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-        phoneNumber: joi.number()
-            .min(9),
+      name: joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+      password: joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+      email: joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+      phoneNumber: joi.number()
+        .min(9),
     })
-    .unknown();
+      .unknown();
     const value = await bodySchema.validateAsync(req.body).catch((err) => err);
-     if (value instanceof Error) {
+    if (value instanceof Error) {
       return res.status(403).json({ error: { message: " No add this user " } });
-   }
+    }
     //check user
     const foundUser = await Users.findOne({ email });
-    
+
     if (foundUser) return res.status(403).json({ error: { message: " Email is already in use " } });
-    
+
     // create new user
     const newUser = new Users({ name, password, email, role })
     newUser.save();
@@ -47,4 +48,9 @@ exports.authentication = catchAsync(async (req, res) => {
     const token = encodeToken(newUser._id);
     res.setHeader("Authorization", token);
     return res.status(201).json({ success: true }); 
+      
+    } catch (error) {
+    next(error);
+    }
+    
 }
